d450f65224d5c91b7463b05a9e6ad473eb433d0a
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-05-18 11:03:39 -0400

    updated haproxy
    Signed-off-by: rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>

--- aws/k8s_resources/route53_private.tf
+++ aws/k8s_resources/route53_private.tf
@@ -16,8 +16,8 @@ resource "aws_route53_record" "private_record_vault" {
   name = var.aws_env != "prod" ? "vault.${local.private_domain}" : "vault.${local.private_domain}"
   type    = "A"
   alias {
-    name                   = data.aws_alb.blk_nlb_internal.dns_name
-    zone_id                = data.aws_alb.blk_nlb_internal.zone_id
+    name                   = data.aws_alb.blk_nlb_external.dns_name
+    zone_id                = data.aws_alb.blk_nlb_external.zone_id
     evaluate_target_health = true
   }
 }
@@ -30,8 +30,8 @@ resource "aws_route53_record" "private_record_aais" {
   zone_id = data.aws_route53_zone.private_zone.zone_id
   alias {
     evaluate_target_health = true
-    name = data.aws_alb.blk_nlb_internal.dns_name
-    zone_id = data.aws_alb.blk_nlb_internal.zone_id
+    name = data.aws_alb.blk_nlb_external.dns_name
+    zone_id = data.aws_alb.blk_nlb_external.zone_id
   }
 }
 #Setting up private dns entries common for all node types
@@ -42,8 +42,8 @@ resource "aws_route53_record" "private_record_common" {
   zone_id = data.aws_route53_zone.private_zone.zone_id
   alias {
     evaluate_target_health = true
-    name = data.aws_alb.blk_nlb_internal.dns_name
-    zone_id = data.aws_alb.blk_nlb_internal.zone_id
+    name = data.aws_alb.blk_nlb_external.dns_name
+    zone_id = data.aws_alb.blk_nlb_external.zone_id
   }
 }
 
