e8e1e5db248ace00c06028b00bdee24a95b2ca35
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-05-23 19:14:02 -0400

    updated eks cluster

--- aws/aws_resources/modules/eks_cluster/variables.tf
+++ aws/aws_resources/modules/eks_cluster/variables.tf
@@ -91,10 +91,6 @@ variable "subnets" {
   description = "A list of subnets to place the EKS cluster within."
   type        = list(string)
 }
-variable "subnet_ids" {
-  description = "A list of subnet ids to place the workers within."
-  type        = string
-}
 variable "tags" {
   description = "A map of tags to add to all resources. Tags added to launch configuration or templates override these values for ASG Tags only."
   type        = map(string)
