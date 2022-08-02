2adf4ad8a6deab24ffee242ce7ba610b83c9b2ab
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2022-08-01 23:07:31 -0400

    testing etl

--- aws/aws_resources/etl_idm.tf
+++ aws/aws_resources/etl_idm.tf
@@ -335,6 +335,7 @@ resource "aws_lambda_function" "etl_intake_processor" {
   timeout = "3"
   source_code_hash = base64sha256(filebase64("./resources/openidl-etl-intake-processor.zip"))
   tags = merge(local.tags,{ "name" = "${local.std_name}-openidl-etl-intake-processor"})
+  depends_on = [data.archive_file.etl_intake_processor_zip]
 }
 resource "aws_lambda_function" "etl_success_processor" {
   function_name = "${local.std_name}-openidl-etl-idm-loader"
@@ -349,6 +350,7 @@ resource "aws_lambda_function" "etl_success_processor" {
   timeout = "3"
   source_code_hash = base64sha256(filebase64("./resources/openidl-etl-success-processor.zip"))
   tags = merge(local.tags,{ "name" = "${local.std_name}-openidl-etl-idm-loader"})
+  depends_on = [data.archive_file.etl_success_processor_zip]
 }
 resource "local_file" "config_intake" {
   content = local.config-intake
