f0de5d171ecfdbc9c91e38ede4cb2ccb5c88a554
Surya Lanka <surya.lanka@itpeoplecorp.com>
2022-10-24 16:12:26 -0400

    Fix missing config file in template
    Signed-off-by: Surya Lanka <surya.lanka@itpeoplecorp.com>

--- aws/upload-ui/route53_public.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "upload_ui" {
-  name = "upload.dev.thetech.digital"
-  zone_id = data.aws_route53_zone.public_zone.zone_id
-  type = "CNAME"
-  ttl = "300"
-  records = [aws_s3_bucket.upload_ui.website_endpoint]
-}
-
